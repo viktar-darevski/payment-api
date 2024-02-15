f227291b37a158ad85947c67001ec1e31248c290
Viktar Dareuski <viktar.dareuski@rigelstorm.com>
2024-02-15 09:13:36 +0300

    fix: demonstrate conventional commit naming - fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
             // Make sure that we display JSON responses in API mode
-//            \App\Http\Middleware\ForceJsonResponse::class,
+            \App\Http\Middleware\ForceJsonResponse::class,
         ],
     ];
 
